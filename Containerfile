573115763b8d82a9f295da7b6aa21a83596742a8
Andreas Wachs <andreas@Andreass-MacBook-Pro.local>
2026-05-17 17:59:16 +0200

    feat: refactor for k8s deployment friendliness

---
+++ Containerfile
@@ -0,0 +1,31 @@
+# syntax=docker/dockerfile:1
+
+FROM eclipse-temurin:8-jre
+
+LABEL org.opencontainers.image.version="9.1.13"
+LABEL org.opencontainers.image.title="The Pixelmon Modpack 9.1.13"
+LABEL org.opencontainers.image.description="Minecraft Pixelmon Modpack server"
+LABEL org.opencontainers.image.url="https://github.com/andreaswachs/pixelmon"
+LABEL org.opencontainers.image.source="https://github.com/andreaswachs/pixelmon"
+
+RUN apt-get update && apt-get install -y curl unzip && \
+    rm -rf /var/lib/apt/lists/* && \
+    groupadd -f --gid 1250 minecraft && \
+    useradd --uid 1250 --gid 1250 --home /data --shell /bin/bash minecraft && \
+    mkdir -p /data/world /data/config && \
+    chown -R minecraft:minecraft /data
+
+COPY launch.sh /launch.sh
+RUN chmod +x /launch.sh
+
+USER minecraft
+
+VOLUME ["/data/world", "/data/config"]
+WORKDIR /data
+
+EXPOSE 25565/tcp
+
+ENV MOTD="Pixelmon Server"
+ENV LEVEL="world"
+
+CMD ["/launch.sh"]
--- launch.sh
+++ launch.sh
@@ -4,33 +4,60 @@ set -x
 
 cd /data
 
+WORLD_DIR="/data/world/${LEVEL:-world}"
+mkdir -p "$WORLD_DIR"
+
+# Symlink persistent config files if present in /data/config
+for cfg_file in server.properties ops.json ops.txt whitelist.json banned-ips.json banned-players.json; do
+    if [ -f "/data/config/$cfg_file" ] && [ ! -L "/data/$cfg_file" ]; then
+        rm -f "/data/$cfg_file"
+        ln -sf "/data/config/$cfg_file" "/data/$cfg_file"
+    fi
+done
+
+# EULA: write to config dir if available, symlink to /data
 if ! [[ "$EULA" = "false" ]]; then
-	echo "eula=true" > eula.txt
+    if [ -d /data/config ]; then
+        echo "eula=true" > /data/config/eula.txt
+        ln -sf /data/config/eula.txt /data/eula.txt
+    else
+        echo "eula=true" > /data/eula.txt
+    fi
 else
-	echo "You must accept the EULA to install."
-	exit 99
+    echo "You must accept the EULA to install."
+    exit 99
 fi
 
 if ! [[ -f serverpack9113.zip ]]; then
     rm -fr configs defaultconfigs kubejs libraries mods forge-*.jar server-setup-config.yaml server-start.* serverstarter-*.jar Enigmatica6Server-*.zip
-	rm -fr config defaultconfigs global_data_packs global_resource_packs mods packmenu libraries
-	curl -Lo serverpack9113.zip 'https://edge.forgecdn.net/files/5954/585/serverpack9113.zip' && unzip -u -o 'serverpack9113.zip' -d /data
-	if [[ -d serverpack ]]; then
-	  mv -f serverpack/* /data/
-	  rm -fr serverpack
-  fi
-	chmod u+x Install.sh
-	./Install.sh
+    rm -fr config defaultconfigs global_data_packs global_resource_packs mods packmenu libraries
+    curl -Lo serverpack9113.zip 'https://edge.forgecdn.net/files/5954/585/serverpack9113.zip' && unzip -u -o 'serverpack9113.zip' -d /data
+    if [[ -d serverpack ]]; then
+        mv -f serverpack/* /data/
+        rm -fr serverpack
+    fi
+    chmod u+x Install.sh
+    ./Install.sh
 fi
 
+# Set level-name to the world PVC mount
+sed -i "s/level-name=.*/level-name=${WORLD_DIR//\//\\/}/" /data/server.properties
+
 if [[ -n "$MAX_RAM" ]]; then
-	sed -i "s/maxRam:.*/maxRam: $MAX_RAM/" /data/server-setup-config.yaml
+    sed -i "s/maxRam:.*/maxRam: $MAX_RAM/" /data/server-setup-config.yaml
 fi
 if [[ -n "$MOTD" ]]; then
     sed -i "s/motd\s*=/ c motd=$MOTD" /data/server.properties
 fi
+
+# Write ops to config dir if available
 if [[ -n "$OPS" ]]; then
-    echo $OPS | awk -v RS=, '{print}' > ops.txt
+    if [ -d /data/config ]; then
+        echo $OPS | awk -v RS=, '{print}' > /data/config/ops.txt
+        ln -sf /data/config/ops.txt /data/ops.txt
+    else
+        echo $OPS | awk -v RS=, '{print}' > /data/ops.txt
+    fi
 fi
 
 sed -i 's/server-port.*/server-port=25565/g' server.properties
